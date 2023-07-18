92b0e2b68419a29b24f83f8ed5474fc11d760dfb
Jonny Adcock <jonathan.adcock@and.digital>
2023-07-18 10:23:36 +0100

    added an openapi spec generated by fastapi

--- scripts/generate_openapi.py
+++ scripts/generate_openapi.py
@@ -1,13 +1,18 @@
+import os
+
 from fastapi.openapi.utils import get_openapi
 import yaml
 
 from app.main import app
 
+OUTPUT_DIR = "gateway"
 OUTPUT_FILENAME = "openapi.yaml"
+
+OUTPUT_PATH = os.path.join(os.getcwd(), OUTPUT_DIR, OUTPUT_FILENAME)
 
 
 # Writes the FastApi schema to an output yaml file
-with open(OUTPUT_FILENAME, 'w+') as f:
+with open(OUTPUT_PATH, 'w+') as f:
     yaml.dump(get_openapi(
         title=app.title,
         version=app.version,
