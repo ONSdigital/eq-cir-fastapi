e197940660f4a80dd05603a8de53fd5122af6e6a
Jonny Adcock <jonathan.adcock@and.digital>
2023-07-18 16:39:57 +0100

    custom responses

--- app/models/responses.py
+++ app/models/responses.py
@@ -1,13 +1,30 @@
+from dataclasses import dataclass
+
 from app.config import logging
 
 logger = logging.getLogger(__name__)
 
 
-def bad_request(message):
-    """
-    Model for BAD_REQUEST response
-    :param message: string
-    :return: dict
-    """
-    logger.error(f"bad_request - {message}")
-    return {"status": "error", "message": message}
+@dataclass
+class BadRequest:
+    """Model for a generic bad request response"""
+
+    message: str = "No CI {metadata/schema} found for {parameters}"
+    status: str = "error"
+
+
+@dataclass
+class CiMetadata:
+    """Model for collection instrument metadata"""
+
+    ci_version: int
+    data_version: str
+    form_type: str
+    id: str
+    langugage: str
+    published_at: str
+    schema_version: str
+    sds_schema: str
+    status: str
+    survey_id: str
+    title: str
