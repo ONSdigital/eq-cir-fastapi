d67b472a3387ee65d1442ed571176a756d9764bc
Jonny Adcock <jonathan.adcock@and.digital>
2023-07-20 14:00:39 +0100

    updated tests

--- tests/unit_tests/test_handlers.py
+++ tests/unit_tests/test_handlers.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 from app.handlers import get_ci_metadata_v1, get_ci_metadata_v2
-from app.models.requests import GetCiMetadataV1Params
+from app.models.requests import GetCiMetadataV1Params, GetCiMetadataV2Params
 
 
 @patch("app.handlers.query_ci_metadata")
@@ -9,7 +9,7 @@ class TestGetCiMetadataV1:
     """
     Tests for the `get_ci_metadata_v1` handler
 
-    Calls to `app.repositories.firestore.query_ci_metadata` are mocked out for these tests
+    All calls to `app.repositories.firestore.query_ci_metadata` are mocked out for these tests
     """
 
     mock_form_type = "t"
@@ -64,322 +64,111 @@ class TestGetCiMetadataV2:
     mock_schema_version = "12"
     mock_data_version = "1"
     mock_status = "DRAFT"
-    mock_request_data = {
-        "survey_id": mock_survey_id,
-        "language": mock_language,
-        "form_type": mock_form_type,
-        "title": mock_title,
-        "schema_version": mock_schema_version,
-        "data_version": mock_data_version,
-    }
+
+    mock_ci_list = [
+        {
+            "survey_id": mock_survey_id,
+            "form_type": mock_form_type,
+            "language": mock_language,
+            "title": "test1",
+            "ci_version": 1,
+        },
+        {
+            "survey_id": mock_survey_id,
+            "form_type": mock_form_type,
+            "language": mock_language,
+            "title": "test2",
+            "ci_version": 2,
+        },
+    ]
+
+    # Create a default, empty request (all params set to `None`)
+    query_params = GetCiMetadataV2Params(form_type=None, language=None, status=None, survey_id=None)  # type: ignore [arg-type]
 
     @patch("app.handlers.get_all_ci_metadata")
-    def test_get_ci_metadata_v2_with_no_parameters_with_200_status(self, mocked_get_all_ci_metadata):
+    def test_get_ci_metadata_v2_with_no_parameters_returns_all_ci(self, mocked_get_all_ci_metadata):
         """
         Why am I testing:
             To check that all the CIs are returned when no parameters are supplied.
         What am I testing:
             Data containing multiple CI is returned when no params are passed
         """
+        # Update mocked `get_all_ci_metadata` call to return list of valid ci
+        mocked_get_all_ci_metadata.return_value = self.mock_ci_list
 
-        mock_ci_list = [
-            {
-                "survey_id": self.mock_survey_id,
-                "form_type": self.mock_form_type,
-                "language": self.mock_language,
-                "title": "test1",
-                "ci_version": 1,
-            },
-            {
-                "survey_id": self.mock_survey_id,
-                "form_type": self.mock_form_type,
-                "language": self.mock_language,
-                "title": "test2",
-                "ci_version": 2,
-            },
-        ]
+        items = get_ci_metadata_v2(self.query_params)
+        assert items == self.mock_ci_list
 
-        mocked_get_all_ci_metadata.return_value = mock_ci_list
-
-        items = get_ci_metadata_v2(mock_request)
-        expected_response = (
-            good_response_200(mock_ci_list),
-            200,
-        )
-
-        assert expected_response == items
-
-
-    def test_get_ci_metadata_v2_with_no_parameters_with_404_status(mocker):
+    @patch("app.handlers.get_all_ci_metadata")
+    def test_get_ci_metadata_v2_with_no_parameters_returns_none(self, mocked_get_all_ci_metadata):
         """
         Why am I testing:
-            To check that a 404 error is returned if no CI's exist.
+            To check that `None` is returned if no CIs are found when no parameters are supplied.
         What am I testing:
-                404 that the CI could not be found and a "message": "No CI metadata found."
+            None is returned when no params are passed
         """
+        # Update mocked `get_all_ci_metadata` call to return `None`
+        mocked_get_all_ci_metadata.return_value = None
 
-        mock_request = {}
+        items = get_ci_metadata_v2(self.query_params)
+        assert items is None
 
-        mocker.patch(
-            "src.handlers.collection_instrument.get_all_ci_metadata",
-            return_value=None,
-        )
-
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_response = (
-            bad_request(
-                f"No CI metadata found for {mock_request.args}",
-            ),
-            404,
-        )
-        assert expected_response == items
-
-
-    # Test for status param ONLY
-
-
-    def test_get_ci_metadata_v2_returns_ci_found_when_querying_with_status_return_200(mocker):
+    @patch("app.handlers.query_ci_by_status")
+    def test_get_ci_metadata_v2_returns_ci_found_when_querying_with_status(self, mocked_query_ci_by_status):
         """
         Why am I testing:
             To check that all the CIs are returned when querying with Status parameter.
         What am I testing:
-            200 success message and data containing multiple CI is returned when status param is passed
+            Data containing multiple CI is returned when status param is passed
         """
-        mock_request = {
-            "status": mock_status,
-        }
-        mock_ci_list = [
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test1",
-                "ci_version": 1,
-                "status": mock_status,
-            },
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test2",
-                "ci_version": 2,
-                "status": mock_status,
-            },
-        ]
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_by_status",
-            return_value=mock_ci_list,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_resp = (good_response_200(mock_ci_list), 200)
-        assert items == expected_resp
+        # Update mocked `get_ci_by_status` call to return list of valid ci
+        mocked_query_ci_by_status.return_value = self.mock_ci_list
 
+        # Update `query_params` to include valid `status`
+        self.query_params.status = self.mock_status
 
-    def test_get_ci_metadata_v2_returns_ci_not_found_when_querying_with_status_return_404(mocker):
-        """
-        Why am I testing:
-            To check that a 404 error is returned if no CI's exist.
-        What am I testing:
-                404 that the CI could not be found and a "message": "No CI metadata found."
-        """
-        mock_request = {
-            "status": mock_status,
-        }
-
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_by_status",
-            return_value=None,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_resp = (
-            bad_request(
-                f"No CI metadata found for {mock_request.args}",
-            ),
-            404,
-        )
-        assert items == expected_resp
-
+        items = get_ci_metadata_v2(self.query_params)
+        assert items == self.mock_ci_list
 
     # Test for survey_id, language, form_type and status params
-
-
-    def test_get_ci_metadata_v2_returns_ci_found_when_querying_survey_lan_formtype_status_return_200(mocker):
+    @patch("app.handlers.query_ci_metadata")
+    def test_get_ci_metadata_v2_returns_ci_found_when_querying_survey_lan_formtype_status(self, mocked_query_ci_metadata):
         """
         Why am I testing:
-            To check that CIs are returned when survey_id, form_type, language, and status are supplied.
+            To check that CIs are returned when survey_id, form_type, language, and status are
+            supplied.
         What am I testing:
-            200 success message and data containing multiple CI is returned when survey_id, form_type,
-            language, and status are passed
+            Data containing multiple CI is returned when survey_id, form_type, language, and status
+            are passed
         """
+        # Update mocked `query_ci_metadata` call to return list of valid ci
+        mocked_query_ci_metadata.return_value = self.mock_ci_list
 
-        mock_request = {
-            "survey_id": mock_survey_id,
-            "form_type": mock_form_type,
-            "language": mock_language,
-            "status": mock_status,
-        }
-        mock_ci_list = [
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test1",
-                "ci_version": 1,
-                "status": mock_status,
-            },
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test2",
-                "ci_version": 2,
-                "status": mock_status,
-            },
-        ]
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_metadata",
-            return_value=mock_ci_list,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
+        # Update `query_params` to include all valid params
+        self.query_params.form_type = self.mock_form_type
+        self.query_params.language = self.mock_language
+        self.query_params.status = self.mock_status
+        self.query_params.survey_id = self.mock_survey_id
 
-        expected_resp = (good_response_200(mock_ci_list), 200)
-        assert items == expected_resp
-
-
-    def test_get_ci_metadata_v2_returns_ci_not_found_when_querying_survey_lan_formtype_status_return_404(mocker):
-        """
-        Why am I testing:
-            To check that there is no CI available by survey_id, form_type,language and status.
-            It should 404 that the CI could not be found.
-        What am I testing:
-        404 that the CI could not be found and a "message": "No CI metadata found for <survey_id> ,
-        <form_type>, <language>, <status> and <version>"
-        """
-        mock_request = {
-            "survey_id": mock_survey_id,
-            "form_type": mock_form_type,
-            "language": mock_language,
-            "status": mock_status,
-        }
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_metadata",
-            return_value=None,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-
-        expected_resp = (
-            bad_request(
-                f"No CI metadata found for {mock_request.args}",
-            ),
-            404,
-        )
-        assert items == expected_resp
-
+        items = get_ci_metadata_v2(self.query_params)
+        assert items == self.mock_ci_list
 
     # Test for survey_id, language, form_type params
-    def test_get_ci_metadata_v2_returns_ci_found_when_querying_with_survey_lan_formtype_returns_200(mocker):
+    @patch("app.handlers.query_ci_metadata")
+    def test_get_ci_metadata_v2_returns_ci_found_when_querying_with_survey_lan_formtype(self, mocked_query_ci_metadata):
         """
         Why am I testing:
             To check that CIs are returned when survey_id, form_type, and language are supplied.
         What am I testing:
-            200 success message and data containing multiple CI is returned when survey_id, form_type, and language are passed
+            Data containing multiple CI is returned when survey_id, form_type, and language are passed
         """
+        # Update mocked `query_ci_metadata` call to return list of valid ci
+        mocked_query_ci_metadata.return_value = self.mock_ci_list
 
-        mock_request = {
-            "survey_id": mock_survey_id,
-            "form_type": mock_form_type,
-            "language": mock_language,
-        }
-        mock_ci_list = [
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test1",
-                "ci_version": 1,
-                "status": mock_status,
-            },
-            {
-                "survey_id": mock_survey_id,
-                "form_type": mock_form_type,
-                "language": mock_language,
-                "title": "test2",
-                "ci_version": 2,
-                "status": mock_status,
-            },
-        ]
+        # Update `query_params` to include survey_id, language, form_type params
+        self.query_params.form_type = self.mock_form_type
+        self.query_params.language = self.mock_language
+        self.query_params.survey_id = self.mock_survey_id
 
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_metadata",
-            return_value=mock_ci_list,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_response = (
-            good_response_200(mock_ci_list),
-            200,
-        )
-        assert expected_response == items
-
-
-    def test_get_ci_metadata_v2_returns_ci_not_found_when_querying_with_survey_lan_formtype_returns_404(mocker):
-        """
-        Why am I testing:
-            To check that there is no CI available by survey_id, form_type, and language.
-            It should 404 that the CI could not be found.
-        What am I testing:
-        404 that the CI could not be found and a "message": "No CI metadata found for <survey_id> ,
-        <form_type> and <version>"}
-        """
-        mock_request = {
-            "survey_id": mock_survey_id,
-            "form_type": mock_form_type,
-            "language": mock_language,
-        }
-
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_metadata",
-            return_value=None,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_response = (
-            bad_request(
-                f"No CI metadata found for {mock_request.args}",
-            ),
-            404,
-        )
-        assert expected_response == items
-
-
-    def test_get_ci_metadata_v2_returns_returns_400_with_bad_parameter_set(mocker):
-        """
-        Why am I testing:
-            To check that when an incorrect parameter set is passed,
-            it should 404 that the CI could not be found.
-        What am I testing:
-        400 and a "message": "Bad query parameters"
-        """
-        mock_request = {
-            "survey_id": mock_survey_id,
-            "form_type": mock_form_type,
-        }
-
-        mocker.patch(
-            "src.handlers.collection_instrument.query_ci_metadata",
-            return_value=None,
-        )
-        mock_request = MockRequestArgs(mock_request)
-        items = get_ci_metadata_v2(mock_request)
-        expected_response = (
-            bad_request(
-                "Bad query parameters",
-            ),
-            400,
-        )
-        assert expected_response == items
+        items = get_ci_metadata_v2(self.query_params)
+        assert items == self.mock_ci_list
--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app.models.requests import GetCiMetadataV2Params
 
 
@@ -11,7 +13,18 @@ class TestGetCiMetadataV2Params:
         `params_not_none` class method should return `True` if all param name strings as input
         arguments have a corresponding non-none class attribute value
         """
-        assert self.query_params.params_not_none("form_type", "language", "status", "survey_id") == True
+        assert self.query_params.params_not_none("form_type", "language", "status", "survey_id") is True
+
+    @pytest.mark.parametrize("input_param", ["form_type", "language", "status", "survey_id"])
+    def test_params_not_none_returns_false_when_single_param_none(self, input_param):
+        """
+        `params_not_none` class method should return `False` if any of param name strings as input
+        arguments have a corresponding class attribute value of `None`
+        """
+        # Update `query_params` to contain a single `None` value
+        setattr(self.query_params, input_param, None)
+
+        assert self.query_params.params_not_none("form_type", "language", "status", "survey_id") is False
 
     def test_params_not_none_returns_false_when_all_params_none(self):
         """
@@ -24,11 +37,4 @@ class TestGetCiMetadataV2Params:
         self.query_params.status = None
         self.query_params.survey_id = None
 
-        assert self.query_params.params_not_none("form_type", "language", "status", "survey_id") == False
-
-    def test_params_not_none_returns_false_when_single_param_none(self):
-        """
-        `params_not_none` class method should return `False` if al param name strings as input
-        arguments have a corresponding class attribute value of `None`
-        """
-        pass
+        assert self.query_params.params_not_none("form_type", "language", "status", "survey_id") is False
