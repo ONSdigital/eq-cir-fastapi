ae33c5cd4c179402926a4836b615dda38c02c81d
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2023-08-29 12:01:51 +0100

    Changed config.py

--- app/config.py
+++ app/config.py
@@ -7,17 +7,17 @@ class Settings(BaseSettings):
     """Application settings"""
 
     CI_FIRESTORE_COLLECTION_NAME: str = "ons-collection-instruments"
-    CI_STORAGE_BUCKET_NAME: str = "ons-cir-sandbox-sri"
-    DEFAULT_HOSTNAME: str = "35.244.169.226.nip.io"
-    OAUTH_CLIENT_ID: str = "686467028672-dbm9v8a58nmh6cc8mvs68de6bb3vvqea.apps.googleusercontent.com"
+    CI_STORAGE_BUCKET_NAME: str = "ons-cir-dev"
+    DEFAULT_HOSTNAME: str = "only required for integration tests"
+    OAUTH_CLIENT_ID: str = "only required for integration tests"
     LOG_LEVEL: str = "INFO"
-    PROJECT_ID: str = "ons-cir-sandbox-sri"
+    PROJECT_ID: str = "ons-cir-dev"
     PUBLISHED_AT_FORMAT: str = "%Y-%m-%dT%H:%M:%S.%fZ"
     PUBSUB_EMULATOR_HOST: str = "only required for development environment"
     STORAGE_EMULATOR_HOST: str = "only required for development environment"
     SUBSCRIPTION_ID: str = "ons-cir-publish-events-subscription-cir"
     TOPIC_ID: str = "ons-cir-publish-ci"
-    URL_SCHEME: str = "https"
+    URL_SCHEME: str = "only required for integration tests"
 
 
 settings = Settings()
