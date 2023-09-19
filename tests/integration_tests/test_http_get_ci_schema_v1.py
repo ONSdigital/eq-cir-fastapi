028c1a8630d6a3053cba28121b6d8cd60292709b
Jonny Adcock <jonathan.adcock@and.digital>
2023-09-19 10:28:17 +0100

    added missing tests for http_get_ci_schema_v1 endpoint

---
+++ tests/integration_tests/test_http_get_ci_schema_v1.py
@@ -0,0 +1,51 @@
+from dataclasses import asdict
+from urllib.parse import urlencode
+
+from fastapi import status
+
+from app.events.subscriber import Subscriber
+from app.models.requests import GetCiSchemaV1Params
+from tests.integration_tests.utils import delete_docs, make_iap_request, post_ci_v1
+
+
+class TestHttpGetCiSchemaV1:
+    """Tests for the `http_get_ci_schema_v1` endpoint"""
+
+    # Initialise the subscriber client
+    subscriber = Subscriber()
+    url = "/v1/retrieve_collection_instrument"
+
+    def teardown_method(self):
+        """Tidy up carried out at the end of each test"""
+        # Need to pull and acknowledge messages in any test where post_ci_v1 is called so the
+        # subscription doesn't get clogged
+        self.subscriber.pull_messages_and_acknowledge()
+        delete_docs("3456")
+
+    def test_endpoint_returns_400_bad_request_if_bad_query(self):
+        """
+        `http_get_ci_schema_v1` should return `HTTP_400_BAD_REQUEST` status if a bad query is made
+        via a GET request
+        """
+        # Create a bad querystring
+        querystring = urlencode({"my_bad": "querystring"})
+
+        response = make_iap_request("GET", f"{self.url}?{querystring}")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_endpoint_returns_200_success_if_ci_schema_found(self, setup_payload):
+        """
+        `http_get_ci_schema_v1` should return `HTTP_200_OK` status if valid ci metadata and schema
+        exist and a valid query to return the schema is made via a GET request
+        """
+        # Use `post_ci_v1` to create ci metadata and schema on the db
+        post_ci_v1(setup_payload)
+
+        # Construct the querystring to retrieve newly created ci schema
+        query_params = GetCiSchemaV1Params(
+            form_type=setup_payload["form_type"], language=setup_payload["language"], survey_id=setup_payload["survey_id"]
+        )
+        querystring = urlencode(asdict(query_params))
+
+        response = make_iap_request("GET", f"{self.url}?{querystring}")
+        assert response.status_code == status.HTTP_200_OK
