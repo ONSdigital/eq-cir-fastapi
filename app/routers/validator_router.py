ef789c353dde927664c81335ae8776386a5a0e7a
Jacob Harrison <35296832+JacobHarrisonONS@users.noreply.github.com>
2025-11-03 17:19:16 +0000

    Put validator version (#86)
    * update validator version and CI
    
    * update openapi, add tests for update validator and ci endpoint
    
    * temp remove audit
    
    * lint
    
    * code review
    
    * update new validator version
    
    * unit tests failing
    
    * Fix unit tests, assert function called with right param
    
    * update inputs
    
    * update postman import
    
    * update units tests and pass on updated metadata object
    
    ---------
    
    Co-authored-by: Isaiah Lai_ons <Isaiah.Lai@ons.gov.uk>

--- app/routers/validator_router.py
+++ app/routers/validator_router.py
@@ -7,6 +7,7 @@ from app.exception import exceptions
 from app.exception.exception_response_models import ExceptionResponseModel
 from app.models.requests import (
     PatchValidatorVersionV1Params,
+    PostCiSchemaV1Data,
 )
 from app.models.responses import CiMetadata
 from app.services.ci_processor_service import CiProcessorService
@@ -75,3 +76,54 @@ async def http_patch_ci_validator_version_v1(
                  [query_params.guid, query_params.validator_version])
 
     return ci_metadata.model_dump()
+
+@router.put(
+    "/v1/update_validator_version",
+    responses={
+        200: {
+            "model": CiMetadata,
+            "description": (
+                    "Successfully updated validator version"
+            ),
+        },
+        500: {
+            "model": ExceptionResponseModel,
+            "content": {"application/json": {"example": erm.erm_500_global_exception}},
+        },
+        404: {
+            "model": ExceptionResponseModel,
+            "content": {"application/json": {"example": erm.erm_404_no_ci_exception}},
+        },
+        400: {
+            "model": ExceptionResponseModel,
+            "content": {"application/json": {"example": erm.erm_400_incorrect_key_names_exception}},
+        },
+    },
+)
+async def http_put_ci_validator_version_v1(
+        post_data: PostCiSchemaV1Data,
+        query_params: PatchValidatorVersionV1Params = Depends(),
+        ci_processor_service: CiProcessorService = Depends(),
+):
+
+    if not query_params.params_not_none(query_params.__dict__.keys()):
+        raise exceptions.ExceptionIncorrectKeyNames
+
+    ci_metadata = ci_processor_service.get_ci_metadata_with_id(query_params.guid)
+
+    if not ci_metadata:
+        error_message = "patch_ci_validator: exception raised - No collection instrument metadata found"
+        logger.error(error_message)
+        logger.debug(f"{error_message}:{query_params.guid}")
+        raise exceptions.ExceptionNoCIMetadata
+
+    if ci_metadata.validator_version == query_params.validator_version:
+        logger.info("No change to validator_version")
+        return ci_metadata.model_dump()
+
+    ci_updated_metadata = ci_metadata.copy()
+    ci_updated_metadata.validator_version = query_params.validator_version
+
+    ci_processor_service.update_validator_version_and_ci(post_data, ci_updated_metadata)
+
+    return ci_updated_metadata.model_dump()
