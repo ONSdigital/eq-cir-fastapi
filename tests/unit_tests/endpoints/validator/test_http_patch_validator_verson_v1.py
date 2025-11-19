0c2bccdec35bfc579008e4d3503072475a3094c7
Jacob Harrison <35296832+JacobHarrisonONS@users.noreply.github.com>
2025-11-19 14:53:28 +0000

    put validator version test (#89)
    * put validator version test
    
    * exclude scripts
    
    * edit cloudbuild
    
    * add test
    
    * edit data
    
    * extend dectorator
    
    * revert
    
    * recreate published date
    
    * lint
    
    * compare time
    
    * edit name of param object
    
    * lint
    
    * edit comment

--- tests/unit_tests/endpoints/validator/test_http_patch_validator_verson_v1.py
+++ tests/unit_tests/endpoints/validator/test_http_patch_validator_verson_v1.py
@@ -6,7 +6,7 @@ from fastapi import status
 
 from app.config import Settings
 from app.main import app
-from app.models.requests import PatchValidatorVersionV1Params
+from app.models.requests import UpdateValidatorVersionV1Params
 from tests.test_data.ci_test_data import (
     mock_validator_version_v2, mock_id, mock_ci_metadata_v2,
 )
@@ -23,7 +23,7 @@ class TestHttpPatchValidatorVersionV1:
 
     base_url = "/v1/update_validator_version"
 
-    query_params = PatchValidatorVersionV1Params(
+    query_params = UpdateValidatorVersionV1Params(
         guid=mock_id,
         validator_version=mock_validator_version_v2
 
