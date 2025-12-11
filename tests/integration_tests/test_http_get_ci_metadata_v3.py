dccc3c8601d4c86b976364afc16405945476ff42
Isaiah Lai_ons <Isaiah.Lai@ons.gov.uk>
2025-12-11 15:21:08 +0000

    Fix typo

--- tests/integration_tests/test_http_get_ci_metadata_v3.py
+++ tests/integration_tests/test_http_get_ci_metadata_v3.py
@@ -4,12 +4,11 @@ import pytest
 from fastapi import status
 
 from app.config import settings
-from app.services.ci_classifier_service import CiClassifierService
 from tests.integration_tests.utils import make_iap_request
 
 
-class TestGetCiMetadataV2:
-    """Tests for the `http_get_ci_metadata_v2` endpoint."""
+class TestGetCiMetadataV3:
+    """Tests for the `http_get_ci_metadata_v3` endpoint."""
 
     base_url = "/v3/ci_metadata"
     post_url = "/v1/publish_collection_instrument"
@@ -32,10 +31,10 @@ class TestGetCiMetadataV2:
 
         guid = post_response.json()["guid"]
 
-        get_ci_metadata_v2_payload = {
+        get_ci_metadata_v3_payload = {
             "guid": guid
         }
-        querystring = urlencode(get_ci_metadata_v2_payload)
+        querystring = urlencode(get_ci_metadata_v3_payload)
 
         # sends request to http_get_ci_metadata_v3 endpoint for data
         get_ci_metadata_v3_response = make_iap_request("GET", f"{self.base_url}?{querystring}")
@@ -65,10 +64,10 @@ class TestGetCiMetadataV2:
         assert query_ci_response["message"] == "No results found"
         assert query_ci_response["status"] == "error"
 
-    def test_metadata_query_ci_v2_returns_unauthorized_request(self, setup_payload):
+    def test_metadata_query_ci_v3_returns_unauthorized_request(self, setup_payload):
         """
         What am I testing:
-        http_get_ci metadata_v2 should return a 401 unauthorized error if the endpoint is requested with an unauthorized token.
+        http_get_ci metadata_v3 should return a 401 unauthorized error if the endpoint is requested with an unauthorized token.
         """
         if settings.CONF == "local-int-tests":
             pytest.skip("Skipping test_metadata_query_ci_v3_returns_unauthorized_request on local environment")
