f6a6e7b92929d1a186d4cd495cf1a09e2aa2de6e
Lucas Dearsley <ldearsley2@gmail.com>
2026-03-30 15:13:07 +0100

    Update restful endpoints

--- app/routers/validator_router_restful.py
+++ app/routers/validator_router_restful.py
@@ -17,66 +17,6 @@ logger = logging.getLogger(__name__)
 settings = Settings()
 
 
-@router.patch(
-    "/v1/collection-instruments/validator-version",
-    responses={
-        200: {
-            "model": CiMetadata,
-            "description": (
-                    "Successfully updated validator version"
-            ),
-        },
-        500: {
-            "model": ExceptionResponseModel,
-            "content": {"application/json": {"example": erm.erm_500_global_exception}},
-        },
-        404: {
-            "model": ExceptionResponseModel,
-            "content": {"application/json": {"example": erm.erm_404_no_ci_exception}},
-        },
-        400: {
-            "model": ExceptionResponseModel,
-            "content": {"application/json": {"example": erm.erm_400_incorrect_key_names_exception}},
-        },
-    },
-)
-async def patch_collection_instrument_validator_version(
-        query_params: UpdateValidatorVersionV1Params = Depends(),
-        ci_processor_service: CiProcessorService = Depends(),
-):
-    """
-    PATCH method that updates validator_version by Guid.
-    """
-    logger.info("Patching validator_version")
-    logger.debug(f"Input data: guid={query_params.guid}, query_params={query_params.__dict__}")
-
-    # Use guid from path, but allow fallback to query param for compatibility
-    if not query_params.params_not_none(query_params.__dict__.keys()):
-        raise exceptions.ExceptionIncorrectKeyNames
-
-    ci_metadata = ci_processor_service.get_ci_metadata_with_id(query_params.guid)
-
-    if not ci_metadata:
-        error_message = "patch_collection_instrument_validator_version: exception raised - No collection instrument metadata found"
-        logger.error(error_message)
-        logger.debug(f"{error_message}:{query_params.guid}")
-        raise exceptions.ExceptionNoCIMetadata
-
-    if ci_metadata.validator_version == query_params.validator_version:
-        logger.info("No change to validator_version")
-        return ci_metadata.model_dump()
-
-    ci_metadata.validator_version = query_params.validator_version
-
-    ci_processor_service.update_ci_validator_version(query_params.guid, ci_metadata)
-
-    logger.info("Metadata successfully updated with new validator_version")
-    logger.debug("Metadata {%1} successfully updated with validator_version: {%2}",
-                 [query_params.guid, query_params.validator_version])
-
-    return ci_metadata.model_dump()
-
-
 @router.put(
     "/v1/collection-instruments/validator-version",
     responses={
